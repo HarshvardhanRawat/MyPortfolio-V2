7ee2a776c131c38ba66a2451c011834c8a385263
Harshvardhan Rawat <fest.nov01@gmail.com>
2026-05-23 14:38:45 +0530

    Completed Gallery Page

--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -5,11 +5,11 @@ import './gallery.css';
 const galleryItems = [
   {
     id: 1,
-    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779477363/IMG_0603_2_kl72bc.jpg'
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779475609/IMG_0761_yrqqx8.jpg'
   },
   {
     id: 2,
-    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779475609/IMG_0761_yrqqx8.jpg'
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779477363/IMG_0603_2_kl72bc.jpg'
   },
   {
     id: 3,
@@ -25,7 +25,7 @@ const galleryItems = [
   },
   {
     id: 6,
-    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779475618/IMG_9403_qka83x.jpg'
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779526523/IMG_0254_le0wym.jpg'
   },
   {
     id: 7,
@@ -33,17 +33,36 @@ const galleryItems = [
   },
   {
     id: 8,
-    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779475618/IMG_9403_qka83x.jpg'
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779526530/IMG_0975_e2arux.jpg'
   },
   {
     id: 9,
-    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779476602/IMG_1853_gt1paj.jpg'
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779476601/IMG_9821_pzhzfn.jpg'
   },
   {
     id: 10,
-    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779476599/IMG-20260414-WA0182_lurk9s.jpg'
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779476612/IMG_9482_tckasd.jpg'
   },
-
+  {
+    id: 11,
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779527174/IMG_0656_chvp57.jpg'
+  },
+  {
+    id: 12,
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779527168/IMG_3143_tnjk02.jpg'
+  },
+  {
+    id: 13,
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779527169/IMG_3669_vxsinw.jpg'
+  },
+  {
+    id: 14,
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779476602/IMG_1853_gt1paj.jpg'
+  },
+  {
+    id: 15,
+    image: 'https://res.cloudinary.com/dph28qrrx/image/upload/v1779476599/IMG-20260414-WA0182_lurk9s.jpg'
+  }
 
 ];
 
